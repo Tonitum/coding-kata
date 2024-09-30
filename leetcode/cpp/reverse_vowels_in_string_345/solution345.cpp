b8fc099bd7a387271165c781284e55bdffc56e8f
Tonitum <nighthunterthexiii@gmail.com>
2024-09-29 20:51:41 -0600

    fix(cpp): more cpp structure

--- leetcode/cpp/reverse_vowels_in_string_345/solution345.cpp
+++ leetcode/cpp/reverse_vowels_in_string_345/solution345.cpp
@@ -1,5 +1,15 @@
 #pragma once
-#include <iostream>
-int main() {
-  std::cout << "Hello World" << std::endl;
-}
+#include <string>
+
+/*
+Given a string s, reverse only all the vowels in the string and return it.
+ The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower
+ and upper cases, more than once.
+*/
+
+class Solution345 {
+public:
+  std::string reverseVowels(std::string s) {
+    return "";
+  }
+};
--- leetcode/cpp/reverse_vowels_in_string_345/test/test_solution_345.cpp
+++ leetcode/cpp/reverse_vowels_in_string_345/test/test_solution_345.cpp
@@ -1,3 +1,8 @@
 #include <gtest/gtest.h>
+#include "reverse_vowels_in_string_345/solution345.cpp"
 
+TEST(TestSolution345, TestCaseOne) {
+  std::string inputString = "IceCreAm";
+  std::string expectedOutput = "AceCreIm";
 
+}
